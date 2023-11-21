dc05b368d0fc7a4dbc1a06353695b70cff1c4e51
Matheus-lima-moreira <6matheuslima@gmail.com>
2023-11-21 17:39:11 -0300

    feat: create get unit service

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { GetUnitsService } from '../../services/get-units.service';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
@@ -17,11 +18,10 @@ export class FormsComponent {
   results = [];
   formGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
-
-  }
+  constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) { }
 
   ngOnInit(): void {
+    this.unitService.getAllUnits().subscribe(data => console.log(data));
     this.formGroup = this.formBuilder.group({
       hour: '',
       showClosed: false,
