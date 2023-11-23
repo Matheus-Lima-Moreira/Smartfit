4315907552885c2286db6f7d7bf65404289dbd76
Matheus-lima-moreira <6matheuslima@gmail.com>
2023-11-23 15:24:54 -0300

    fix: update location type

--- src/app/types/location.interface.ts
+++ src/app/types/location.interface.ts
@@ -3,11 +3,15 @@ import { Schedule } from "./schedule.interface"
 export interface Location {
   id: number,
   title: string,
-  content: string,
-  opened: boolean,
-  mask: string,
-  towel: string,
-  fountain: string,
-  locker_room: string,
+  content?: string,
+  opened?: boolean,
+  mask?: string,
+  towel?: string,
+  fountain?: string,
+  locker_room?: string,
+  uf?: string,
+  street?: string,
+  state_name?: string,
+  region?: string,
   schedules: Schedule[]
 }
