966958bb0e4b226c74ee8d0d22e95cfa43dcb22d
Matheus-lima-moreira <6matheuslima@gmail.com>
2023-11-25 16:58:29 -0300

    feat: creating footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { CardsListComponent } from './components/cards-list/cards-list.component
 import { Location } from './types/location.interface';
 import { GetUnitsService } from './services/get-units.service';
 import { LegendComponent } from './components/legend/legend.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +19,8 @@ import { LegendComponent } from './components/legend/legend.component';
     HeaderComponent,
     FormsComponent,
     CardsListComponent,
-    LegendComponent
+    LegendComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
