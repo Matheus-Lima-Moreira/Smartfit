864fa0e147f8e418dc1f7bbd0cc5cd76b130dfc6
Matheus-lima-moreira <6matheuslima@gmail.com>
2023-11-25 16:57:42 -0300

    fix: filter not working properly

--- src/app/services/filter-units.service.ts
+++ src/app/services/filter-units.service.ts
@@ -7,11 +7,11 @@ const OPENNING_HOURS = {
     end: '12',
   },
   afternoon: {
-    start: '12',
+    start: '12.01',
     end: '18',
   },
   night: {
-    start: '18',
+    start: '18.01',
     end: '23',
   },
 };
@@ -37,10 +37,10 @@ export class FilterUnitsService {
   }
 
   filterUnits(unit: Location, open_hour: string, close_hour: string): boolean {
-    if(!unit.schedules) return true;
+    if (!unit.schedules) return true;
 
-    let open_hour_number = parseInt(open_hour);
-    let close_hour_number = parseInt(close_hour);
+    let open_hour_number = parseFloat(open_hour);
+    let close_hour_number = parseFloat(close_hour);
 
     const todays_weekday = this.transformWeekDay(new Date().getDay());
 
@@ -51,14 +51,15 @@ export class FilterUnitsService {
       if (schedule_weekday === todays_weekday) {
         if (schedule_hour !== 'Fechada') {
           let [unit_open_hour, unit_close_hour] = schedule_hour.split(' às ');
-          let unit_open_hour_number = parseInt(unit_open_hour.replace('h', ''));
-          let unit_close_hour_number = parseInt(unit_close_hour.replace('h', ''));
+          let unit_open_hour_number = parseFloat(unit_open_hour.replace('h', ''));
+          let unit_close_hour_number = parseFloat(unit_close_hour.replace('h', ''));
 
-          if (unit_open_hour_number <= open_hour_number && unit_close_hour_number >= close_hour_number) {
-            return true;
-          } else {
-            return false;
-          }
+          const unit_covers_all_time = unit_open_hour_number <= open_hour_number && unit_close_hour_number >= close_hour_number;
+          const unit_covers_open_time = unit_open_hour_number <= open_hour_number && unit_close_hour_number >= open_hour_number;
+          const unit_covers_close_time = unit_open_hour_number <= close_hour_number && unit_close_hour_number >= close_hour_number;
+          const unit_covers_open_and_close_time = unit_open_hour_number >= open_hour_number && unit_close_hour_number <= close_hour_number;
+
+          return unit_covers_all_time || unit_covers_open_time || unit_covers_close_time || unit_covers_open_and_close_time;
         }
       }
     }
@@ -66,7 +67,7 @@ export class FilterUnitsService {
     return false;
   }
 
-  filter (results: Location[], showClosed: boolean, hour: string) {
+  filter(results: Location[], showClosed: boolean, hour: string) {
     let filteredResults = results;
 
     if (!showClosed) {
